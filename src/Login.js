7c19a2e0aa62f68a84bc6f99d8ebcbfc76bfa9d3
@somitChainintel <somit@chainintel.ai>
2023-05-09 09:21:16 +0545

    Update on style

--- src/Login.js
+++ src/Login.js
@@ -1,94 +1,80 @@
 import React from "react";
-import  ReactDOM  from "react-dom";
+import ReactDOM from "react-dom";
 // import Login from"./Login"
-import usersData from "./users.json"
-import "./App.css"
-import TextField from '@mui/material/TextField';
-import { Input, InputAdornment, IconButton } from '@mui/material';
-import { VisibilityOff, Visibility } from '@mui/icons-material';
+import usersData from "./users.json";
+import "./App.css";
+import TextField from "@mui/material/TextField";
+import { Input, InputAdornment, IconButton } from "@mui/material";
+import { VisibilityOff, Visibility } from "@mui/icons-material";
 
+export default function Login({ handleLogin }) {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [showPassword, setShowPassword] = React.useState(false);
 
+  function changeUsername(e) {
+    setUsername(e.target.value);
+  }
 
+  function changePassword(e) {
+    setPassword(e.target.value);
+  }
 
-export default function Login({handleLogin}){
-    const[username,setUsername]=React.useState("")
-    const[password,setPassword]=React.useState("")
-    const [showPassword, setShowPassword] = React.useState(false);
-
-
-    function changeUsername(e){
-        setUsername(e.target.value)
+  function handleSubmit(e) {
+    e.preventDefault();
+    const user = usersData.users.find(
+      (data) => data.username === username && data.password === password
+    );
+    if (user) {
+      handleLogin(user);
+    } else {
+      alert("invalid opereations ");
     }
+  }
 
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
-    function changePassword(e){
-        setPassword(e.target.value)
-    }
-    function handleSubmit(e){
-        e.preventDefault();
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
 
-        const user= usersData.users.find((data)=>data.username===username&& data.password===password);
+  return (
+    <main className="mainDesign">
+      <div classname="logoSide">
+        <img src={require("./logo.png")} className="logoStyleLogin"></img>
+        <p className="taglineStyle"> Build UI Components 10x faster</p>
+      </div>
 
-        if (user){
-            handleLogin(user)
-        }
-        else {
-            alert("invalid opereations ")
-        }
-    }
-    const handleClickShowPassword = () => {
-        setShowPassword(!showPassword);
-      };
-      
-      const handleMouseDownPassword = (event) => {
-        event.preventDefault();
-      };
-      
+      <div className="loginFormStyle">
+        <form onSubmit={handleSubmit} autoComplete="off">
+          <login className="loginStyle">LOGIN</login>
 
+          <label htmlFor="username" className="labelStyle">
+            {" "}
+            <star className="starStyle">*</star>Username
+          </label>
 
+          <input
+            type="text"
+            className="textFieldStyle"
+            id="username"
+            value={username}
+            onChange={changeUsername}
+          />
+          <br></br>
 
+          <label htmlFor="password" className="labelStyle">
+            {" "}
+            <star className="starStyle">*</star>Password
+          </label>
 
-    return(
-        <main className="mainDesign">
-
-
-              <div classname="logoSide">
-                <img src={require("./logo.png")} className="logoStyleLogin"></img>
-                <p className="taglineStyle"> Build UI Components 10x faster</p>
-            </div>
-
-
-            <div className="loginFormStyle">
-
-            
-            <form onSubmit={handleSubmit} autoComplete="off">
-             
-            <login className="loginStyle">
-              LOGIN
-            </login>
-            
-            
-
-            <label htmlFor="username" className="labelStyle"> <star className="starStyle">
-                *
-            </star>Username</label>
-           
-            
-            <input type="text" className="textFieldStyle" id="username"   value={username} onChange={changeUsername} />
-            <br>
-            </br>
-
-            
-           
-            <label htmlFor="password" className="labelStyle"> <star className="starStyle">
-                *
-            </star>Password</label>
-            
-            
-            
-
-            <Input className="textFieldStyle" value={password} 
-            type={showPassword ? 'text' : 'password'} onChange={changePassword}
+          <Input
+            className="textFieldStyle"
+            value={password}
+            type={showPassword ? "text" : "password"}
+            onChange={changePassword}
             endAdornment={
               <InputAdornment position="end">
                 <IconButton
@@ -102,34 +88,35 @@ export default function Login({handleLogin}){
             }
           />
 
-              <div className="forgetPasswordStyle">
-                <a href="#"> Forget Password? </a>
-              </div>
+          <div className="forgetPasswordStyle">
+            <a href="#"> Forget Password? </a>
+          </div>
 
+          <br></br>
+          <button type="submit" className="loginButtonStyle">
+            {" "}
+            Login{" "}
+          </button>
 
-            
-            <br>
-            </br>
-            <button type="submit" className="loginButtonStyle" > Login </button>
+          <div className="forgetPasswordStyle">
+            <a href="#" id="privacyStyle">
+              {" "}
+              Privacy Policy{" "}
+            </a>
+            <a href="#" id="termsStyle">
+              {" "}
+              Terms and Conditions{" "}
+            </a>
+          </div>
 
-            <div className="forgetPasswordStyle">
-                <a href="#" id="privacyStyle"> Privacy Policy </a>
-                <a href="#" id="termsStyle"> Terms and Conditions </a>
-                
-              </div>
-
-              <div className="forgetPasswordStyle">
-                <a href="#" id="donotStyle"> Do not have an account? </a>
-                
-              </div>
-            
-            </form>
-
-            
-           
-            </div > 
-
-           
-        </main>
-    )
+          <div className="forgetPasswordStyle">
+            <a href="#" id="donotStyle">
+              {" "}
+              Do not have an account?{" "}
+            </a>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
 }
